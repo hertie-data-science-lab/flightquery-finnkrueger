17eaf0d5366218e1b9beb27135dd8cada28e47bc
finnkrueger <fkrueger@inbox.lv>
2023-04-09 21:55:59 +0200

    final final

--- FlightQuery.py
+++ FlightQuery.py
@@ -1,23 +1,66 @@
 from SortedTableMap import *
 
-class FlightQuery(SortedTableMap):
-    '''An application of SortedTableMap, used to query tickets of expeted period'''
-    class Key:
-        __slots__ = "_origin", "_dest", "_date", "_time"
-        pass
 
-    def query(self, k1, k2):
-        pass
+class FlightKey:
+    def __init__(self, origin, dest, date, time):
+        self.origin = origin
+        self.dest = dest
+        self.date = date
+        self.time = time
+
+    def __lt__(self, other):
+        if self.date < other.date:
+            return True
+        elif self.date == other.date and self.time < other.time:
+            return True
+        elif self.date == other.date and self.time == other.time and self.origin < other.origin:
+            return True
+        elif self.date == other.date and self.time == other.time and self.origin == other.origin and self.dest < other.dest:
+            return True
+        else:
+            return False
+
+    def __le__(self, other):
+        return self == other or self < other
+
+
+
+class FlightQuery:
+    def __init__(self):
+        self._flight_data = SortedTableMap()
+
+    def insert_flight(self, flight_key, flight_num):
+        self._flight_data[flight_key] = flight_num
+
+    def delete_flight(self, flight_key):
+        del self._flight_data[flight_key]
+
+    def find_range(self, start_key, end_key):
+        for flight_key, flight_num in self._flight_data:
+            if start_key <= flight_key <= end_key:
+                yield flight_key, flight_num
+
+    def query(self, origin, dest, date, start_time, end_time):
+        start_key = FlightKey(origin, dest, date, start_time)
+        end_key = FlightKey(dest, origin, date, end_time)
+        results = []
+        for flight_key, flight_num in self.find_range(start_key, end_key):
+            if flight_key.origin == origin and flight_key.dest == dest and flight_key.date == date:
+                results.append((flight_key.time, flight_num))
+        return sorted(results)
 
 
 a = FlightQuery()
-s = [("A", "B", 622, 1200, "No1"), ("A", "B", 622, 1230, "No2"), ("A", "B", 622, 1300, "No3")]
-for each in s:
-    key = a.Key(each[0], each[1], each[2], each[3])
-    value = each[4]
-    a[key] = value
-print(len(a))
 
-k1 = ("A", "B", 622, 1200)
-k2 = ("A", "B", 622, 1300)
-a.query(k1, k2)
+a.insert_flight(FlightKey("A", "B", 622, 1200), "No1")
+a.insert_flight(FlightKey("A", "B", 622, 1230), "No2")
+a.insert_flight(FlightKey("A", "B", 622, 1300), "No3")
+
+origin = "A"
+dest = "B"
+date = 622
+time1 = 1200
+time2 = 1300
+
+results = a.query(origin, dest, date, time1, time2)
+print(results)
