ab182da1fb53c53d525955e7020c8035f33f23f8
finnkrueger <fkrueger@inbox.lv>
2023-04-10 21:40:12 +0200

    final_commit

--- FlightQuery.py
+++ FlightQuery.py
@@ -1,66 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 10 21:30:21 2023
+
+@author: finnkruger
+"""
 from SortedTableMap import *
 
-
-class FlightKey:
-    def __init__(self, origin, dest, date, time):
-        self.origin = origin
-        self.dest = dest
-        self.date = date
-        self.time = time
-
-    def __lt__(self, other):
-        if self.date < other.date:
-            return True
-        elif self.date == other.date and self.time < other.time:
-            return True
-        elif self.date == other.date and self.time == other.time and self.origin < other.origin:
-            return True
-        elif self.date == other.date and self.time == other.time and self.origin == other.origin and self.dest < other.dest:
-            return True
-        else:
+class FlightQuery(SortedTableMap):
+    '''An application of SortedTableMap, used to query tickets of expected period'''
+    
+    class Key:
+        __slots__ = "_origin", "_dest", "_date", "_time"
+        def __init__(self, origin, dest, date, time):
+            self._origin = origin
+            self._dest = dest
+            self._date = date
+            self._time = time
+        
+        def __lt__(self, other):
+            if self._origin < other._origin:
+                return True
+            elif self._origin == other._origin:
+                if self._dest < other._dest:
+                    return True
+                elif self._dest == other._dest:
+                    if self._date < other._date:
+                        return True
+                    elif self._date == other._date:
+                        if self._time < other._time:
+                            return True
             return False
+        
+        def __le__(self, other):
+            return self < other or self == other
 
-    def __le__(self, other):
-        return self == other or self < other
+        def __eq__(self, other):
+            return self._origin == other._origin and \
+                   self._dest == other._dest and \
+                   self._date == other._date and \
+                   self._time == other._time
 
-
-
-class FlightQuery:
-    def __init__(self):
-        self._flight_data = SortedTableMap()
-
-    def insert_flight(self, flight_key, flight_num):
-        self._flight_data[flight_key] = flight_num
-
-    def delete_flight(self, flight_key):
-        del self._flight_data[flight_key]
-
-    def find_range(self, start_key, end_key):
-        for flight_key, flight_num in self._flight_data:
-            if start_key <= flight_key <= end_key:
-                yield flight_key, flight_num
-
-    def query(self, origin, dest, date, start_time, end_time):
-        start_key = FlightKey(origin, dest, date, start_time)
-        end_key = FlightKey(dest, origin, date, end_time)
-        results = []
-        for flight_key, flight_num in self.find_range(start_key, end_key):
-            if flight_key.origin == origin and flight_key.dest == dest and flight_key.date == date:
-                results.append((flight_key.time, flight_num))
-        return sorted(results)
-
-
+    def query(self, k1, k2):
+        '''Returns a list of flights whose keys are lexicographically between k1 and k2'''
+        return [(item._key._origin, item._key._dest, item._key._date, item._key._time, item._value) 
+                for item in self._table if k1 <= item._key <= k2]
+    
+    
 a = FlightQuery()
+s = [("A", "B", 622, 1200, "No1"), ("A", "B", 622, 1230, "No2"), ("A", "B", 622, 1300, "No3")]
+for each in s:
+    key = a.Key(each[0], each[1], each[2], each[3])
+    value = each[4]
+    a[key] = value
+print(len(a))
 
-a.insert_flight(FlightKey("A", "B", 622, 1200), "No1")
-a.insert_flight(FlightKey("A", "B", 622, 1230), "No2")
-a.insert_flight(FlightKey("A", "B", 622, 1300), "No3")
-
-origin = "A"
-dest = "B"
-date = 622
-time1 = 1200
-time2 = 1300
-
-results = a.query(origin, dest, date, time1, time2)
-print(results)
+k1 = a.Key("A", "B", 622, 1200)
+k2 = a.Key("A", "B", 622, 1300)
+print(a.query(k1, k2))
